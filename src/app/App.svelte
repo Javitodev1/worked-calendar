b76d3c70fdfa23aeb4ce8b27f08ab9e13030be4c
Javitodev <javito.dev@gmail.com>
2024-05-24 16:52:39 -0400

    april published added

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -20,41 +20,65 @@
       imgSrc: "/publicaciones/abril/06.png",
     },
     '010': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/p/C5oi80DP60w",
+      imgSrc: "/publicaciones/abril/010.png",
     },
     '011': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/p/C5qoAeXOJ11",
+      imgSrc: "/publicaciones/abril/011.png",
     },
     '013': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/p/C5w3SnQuJtC",
+      imgSrc: "/publicaciones/abril/010.png",
     },
     '014': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/p/C5yOmqxOcXi",
+      imgSrc: "/publicaciones/abril/014.png",
     },
     '016': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/reel/C54BOtzvBnM",
+      imgSrc: "/publicaciones/abril/016.png",
     },
     '017': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/p/C56rdSivx",
+      imgSrc: "/publicaciones/abril/017.png",
     },
     '018': {
-      href: "",
-      imgSrc: "",
+      href: "https://www.instagram.com/reel/C59OeVASa6g",
+      imgSrc: "/publicaciones/abril/018.png",
+    },
+    '022': {
+      href: "https://www.instagram.com/p/C6HhdhsvjHE",
+      imgSrc: "/publicaciones/abril/022.png",
+    },
+    '023': {
+      href: "https://www.instagram.com/p/C6Jyw8Cvvmd",
+      imgSrc: "/publicaciones/abril/023.png",
+    },
+    '024': {
+      href: "https://www.instagram.com/reel/C6MowNDuua9",
+      imgSrc: "/publicaciones/abril/024.png",
+    },
+    '025': {
+      href: "https://www.instagram.com/p/C6OySe3ud6h",
+      imgSrc: "/publicaciones/abril/025.png",
+    },
+    '028': {
+      href: "https://www.instagram.com/reel/C6WfADMxMlz",
+      imgSrc: "/publicaciones/abril/028.png",
+    },
+    '029': {
+      href: "https://www.instagram.com/p/C6ZW_hsPFYl",
+      imgSrc: "/publicaciones/abril/029.png",
     },
     '118': {
       href: "",
-      imgSrc: "",
+      imgSrc: "/publicaciones/abril/010.png",
     },
   }
 
   const posts = Object.keys(data).length
-  const imagesPosts = 48
+  // const imagesPosts = 48
 
   const months = [
     {
@@ -154,13 +178,13 @@
         <p class="text-white">Sin publicacion esta fecha</p>
       {:else}
         <img src="{currentData.imgSrc}" alt="publicacion" class="">
-        <a href="{currentData.href}" class="text-sky-600 hover:underline mt-6">Enlace a la publicación</a>
+        <a target="_blank" href="{currentData.href}" class="text-sky-600 hover:underline mt-6">Enlace a la publicación</a>
       {/if}
     </div>
   </div>
 
   <div>
     <p class="text-white">Total publicaciones: <span class="text-sky-600">{posts}</span></p>
-    <p class="text-white">Total de imagenes publicadas: <span class="text-sky-600">{imagesPosts}</span></p>
+    <!-- <p class="text-white">Total de imagenes publicadas: <span class="text-sky-600">{imagesPosts}</span></p> -->
   </div>
 </section>
