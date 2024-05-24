f4ffe17614dc46d4d323a77d4ffadb5f7a478c1a
Javitodev <javito.dev@gmail.com>
2024-05-24 17:51:48 -0400

    added optimized images

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,86 +1,86 @@
 export const data = {
   '06': {
     href: "https://www.instagram.com/reel/C5etbxhs-fN",
-    imgSrc: "/publicaciones/abril/06.png",
+    imgSrc: "/publicaciones/abril/06.webp",
   },
   '010': {
     href: "https://www.instagram.com/p/C5oi80DP60w",
-    imgSrc: "/publicaciones/abril/010.png",
+    imgSrc: "/publicaciones/abril/010.webp",
   },
   '011': {
     href: "https://www.instagram.com/p/C5qoAeXOJ11",
-    imgSrc: "/publicaciones/abril/011.png",
+    imgSrc: "/publicaciones/abril/011.webp",
   },
   '013': {
     href: "https://www.instagram.com/p/C5w3SnQuJtC",
-    imgSrc: "/publicaciones/abril/010.png",
+    imgSrc: "/publicaciones/abril/010.webp",
   },
   '014': {
     href: "https://www.instagram.com/p/C5yOmqxOcXi",
-    imgSrc: "/publicaciones/abril/014.png",
+    imgSrc: "/publicaciones/abril/014.webp",
   },
   '016': {
     href: "https://www.instagram.com/reel/C54BOtzvBnM",
-    imgSrc: "/publicaciones/abril/016.png",
+    imgSrc: "/publicaciones/abril/016.webp",
   },
   '017': {
     href: "https://www.instagram.com/p/C56rdSivx",
-    imgSrc: "/publicaciones/abril/017.png",
+    imgSrc: "/publicaciones/abril/017.webp",
   },
   '018': {
     href: "https://www.instagram.com/reel/C59OeVASa6g",
-    imgSrc: "/publicaciones/abril/018.png",
+    imgSrc: "/publicaciones/abril/018.webp",
   },
   '022': {
     href: "https://www.instagram.com/p/C6HhdhsvjHE",
-    imgSrc: "/publicaciones/abril/022.png",
+    imgSrc: "/publicaciones/abril/022.webp",
   },
   '023': {
     href: "https://www.instagram.com/p/C6Jyw8Cvvmd",
-    imgSrc: "/publicaciones/abril/023.png",
+    imgSrc: "/publicaciones/abril/023.webp",
   },
   '024': {
     href: "https://www.instagram.com/reel/C6MowNDuua9",
-    imgSrc: "/publicaciones/abril/024.png",
+    imgSrc: "/publicaciones/abril/024.webp",
   },
   '025': {
     href: "https://www.instagram.com/p/C6OySe3ud6h",
-    imgSrc: "/publicaciones/abril/025.png",
+    imgSrc: "/publicaciones/abril/025.webp",
   },
   '028': {
     href: "https://www.instagram.com/reel/C6WfADMxMlz",
-    imgSrc: "/publicaciones/abril/028.png",
+    imgSrc: "/publicaciones/abril/028.webp",
   },
   '029': {
     href: "https://www.instagram.com/p/C6ZW_hsPFYl",
-    imgSrc: "/publicaciones/abril/029.png",
+    imgSrc: "/publicaciones/abril/029.webp",
   },
   '10': {
     href: "https://www.instagram.com/p/C6bbNFouW4e",
-    imgSrc: "/publicaciones/mayo/10.png",
+    imgSrc: "/publicaciones/mayo/10.webp",
   },
   '11': {
     href: "https://www.instagram.com/reel/C6ePBWbLAPu",
-    imgSrc: "/publicaciones/mayo/11.png",
+    imgSrc: "/publicaciones/mayo/11.webp",
   },
   '15': {
     href: "https://www.instagram.com/p/C6o_NA0PHLQ",
-    imgSrc: "/publicaciones/mayo/15.png",
+    imgSrc: "/publicaciones/mayo/15.webp",
   },
   '114': {
     href: "https://www.instagram.com/p/C6_uUr4Oy3Y",
-    imgSrc: "/publicaciones/mayo/114.png",
+    imgSrc: "/publicaciones/mayo/114.webp",
   },
   '117': {
     href: "https://www.instagram.com/reel/C7G8T6NgROQ",
-    imgSrc: "/publicaciones/mayo/117.png",
+    imgSrc: "/publicaciones/mayo/117.webp",
   },
   '120': {
     href: "https://www.instagram.com/p/C7PUeNQPwjm",
-    imgSrc: "/publicaciones/mayo/120.png",
+    imgSrc: "/publicaciones/mayo/120.webp",
   },
   '122': {
     href: "https://www.instagram.com/reel/C7UhhDdPmD4",
-    imgSrc: "/publicaciones/mayo/122.png",
+    imgSrc: "/publicaciones/mayo/122.webp",
   },
 }
